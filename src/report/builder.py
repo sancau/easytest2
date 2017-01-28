40720b519bca8996f57f9d79571ea5cb64493b1d
Alexander <alexander.tatchin@gmail.com>
2017-01-28 21:34:59 +0300

    iter: tmode template / builder done todo: hmode template / builder

--- src/report/builder.py
+++ src/report/builder.py
@@ -50,21 +50,26 @@ class ReportBuilder:
                 return '\u0394{} = {} \u00B0C'.format(notation, value)
 
         def get_rows_for_word(sensors):
-            print(sensors)
-            return [
-                {
-                    'i': 1,
-                    'one': 1, 'two': 2, 'three': 3, 'four': 4,
-                    'five': 5, 'six': 6, 'seven': 7, 'eight': 8,
-                    'cp': 9, 'md': 10
-                },
-                {
-                    'i': 2,
-                    'one': 1, 'two': 2, 'three': 3, 'four': 4,
-                    'five': 5, 'six': 6, 'seven': 7, 'eight': 8,
-                    'cp': 9, 'md': 10
-                }
-            ]
+            rows_index_map = {0: 1, 1: 2, 2: 3, 3: 4,
+                              4: 5, 5: 6, 6: 7, 7: 8,
+                              8: 9, 9: 10,
+                              10: 'Средн', 11: 'Т мах', 12: 'T мин',
+                              13: 'А мах', 14: 'А мин'}
+
+            sensor_index_map = {0: 'one', 1: 'two', 2: 'three',
+                                3: 'four', 4: 'five', 5: 'six',
+                                6: 'seven', 7: 'eight', 8: 'nine',
+                                9: 'ten', 10: 'eleven', 11: 'twelve',
+                                12: 'thirteen', 13: 'fourteen',
+                                14: 'fifteen', 15: 'sixteen',
+                                16: 'seventeen', 17: 'eighteen'}
+            rows = []
+            for i in range(len(sensors[0])):
+                r = {'i': rows_index_map[i]}
+                for idx, sensor in enumerate(sensors):
+                    r[sensor_index_map[idx]] = sensor[i]
+                rows.append(r)
+            return rows
 
         mode_vals = mode['processed']['values']
 
