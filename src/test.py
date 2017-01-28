8f9d462e47fc7031f19380052c56bdc768c1e15e
Alexander <alexander.tatchin@gmail.com>
2017-01-28 15:59:17 +0300

    iter: test scenario changed. todo: tmode table template

--- src/test.py
+++ src/test.py
@@ -1,27 +1,79 @@
 # -*- coding: utf8 -*-
 
+from collections import namedtuple
+
 from easytest import Test
-from test_input.temperature import TEST_TEMPERATURE_MODE
-from test_input.humidity import TEST_HUMIDITY_MODE
+from report.builder import ReportBuilder
+
+LogDescriptions = namedtuple('LogDescriptions', ['DT1', 'DT2', 'KT'])
+DESC = LogDescriptions('DT1', 'DT2', 'KT')
+
 
 t = Test()  # user creates TEST
 
 # user adds modes and log files
-t.add_temperature_mode(TEST_TEMPERATURE_MODE)
-t.add_humidity_mode(TEST_HUMIDITY_MODE)
-t.add_temperature_mode(TEST_TEMPERATURE_MODE)
-t.add_humidity_mode(TEST_HUMIDITY_MODE)
-t.add_temperature_mode(TEST_TEMPERATURE_MODE)
-t.add_humidity_mode(TEST_HUMIDITY_MODE)
+TEST_HUMIDITY_MODE = {
+    'logs': [
+        {
+            'file': 'test_data/humidity/1.xlsx',
+            'desc': DESC.DT1
+        },
+        {
+            'file': 'test_data/humidity/2.xlsx',
+            'desc': DESC.DT2
+        },
+        {
+            'file': 'test_data/humidity/2.xlsx',
+            'desc': DESC.KT
+        }
+    ],
+    'target': {
+        'humidity': 90,
+        'temperature': 25
+    },
+    'md': {
+        'humidity': [90, 90, 90, 90, 90, 90, 90, 90, 90, 90],
+        'temperature': [25, 25, 25, 25, 25, 25, 25, 25, 25, 25]
+    }
+}
+
+# user adds modes and log files
+TEST_TEMPERATURE_MODE = {
+    "target": "0",
+    "logs": [
+        {
+            "file": "test_data/0_left.txt",
+            "sensors_count": "8",
+        },
+        {
+            "file": "test_data/0_right.txt",
+            "sensors_count": "8"
+        }
+    ],
+    "cp": [
+        "0", "0", "0", "0", "0",
+        "0", "0", "0", "0", "0"
+    ],
+    "md": [
+        "0.2", "0", "0", "0", "0",
+        "0", "0.3", "0.2", "0", "0.1"
+    ]
+}
+
+# user adds modes to the test object
 t.add_temperature_mode(TEST_TEMPERATURE_MODE)
 t.add_humidity_mode(TEST_HUMIDITY_MODE)
 
 # user adds report data
 t.data['specialist'] = 'Татчин А.В.'
+
+# user adds tools data
 t.data['tools'] = [  # tools data
     'GTH 5050',
     'Гидрометр ФЫ-2'
 ]
+
+# user adds system data
 t.data['system'] = {  # system info
     'name': 'Votsch 7250',
     'year_of_production': '2013',
@@ -32,20 +84,27 @@ t.data['system'] = {  # system info
     'test_method': 'Методика аттестации Votsch 7250 (ГОСТ 34512)',
 }
 
-t.calculate()
+
+t.calculate()  # invoking test calculation logic
+
+# user can save test to file and reload it from the file
 t.path = 'tests/test_1.json'
 t.save()
 print('Calculated and saved to:', t.path)
 
 
 # TEMPLATE
-from report.builder import ReportBuilder
 
+# user specifies output path for report
 report_path = r'C:\Users\2065\Desktop\easytest2\reports'  # ask user every time
 
-builder = ReportBuilder(t, report_path)
 
-builder.build_additions()
-builder.build_main()
+# user can generate report files in MS WORD format
+def make_report():
+    builder = ReportBuilder(t, report_path)
+    builder.build_additions()
+    builder.build_main()
+
+make_report()
 
 print('Reports created.')
