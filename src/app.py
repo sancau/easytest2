150b943389ac91582c1418cf78572bef1255925a
Alexander <alexander.tatchin@gmail.com>
2017-02-23 15:46:00 +0300

    feat: implemented basic menu commands and bl.test interaction logic

--- src/app.py
+++ src/app.py
@@ -1,35 +1,57 @@
 # -*- coding: utf-8 -*-
 
 """
-Easytest2 UI Application
+Easytest 2 UI Application
 """
+import sys
 
 from PyQt5 import uic, QtWidgets as QW
-from PyQt5.QtCore import QTranslator, QObject, QVariant
+from PyQt5.QtCore import QTranslator
 
-import sys
+from bl.easytest import Test
 
 
 class EasyTest(QW.QMainWindow):
     def __init__(self):
         super(EasyTest, self).__init__()
         uic.loadUi('easytest2.ui', self)
+        self.test = None
         self.init_ui()
 
     def init_ui(self):
-        self.test_active = False
         self.setWindowTitle('EasyTest 2')
         self.tabWidget.close()
         self.init_menu()
+        self.bind_test_to_ui()
         self.show()
 
     def init_menu(self):  # initialize top menu
         self.exit.triggered.connect(self.close)
-        self.new_test.triggered.connect(self.init_new_test)
+        self.new_test.triggered.connect(self.init_new_test_handler)
         self.open_test.triggered.connect(self.open_test_handler)
+        self.save.triggered.connect(self.save_test_handler)
+        self.save_as.triggered.connect(self.save_test_as_handler)
 
-    def init_new_test(self):
-        if self.test_active:
+    def bind_test_to_ui(self):
+        def bind(field, value):
+            self.test.data[field] = value
+        self.specialist.textEdited.connect(lambda x: bind('specialist', x))
+
+    # MENU ACTION HANDLERS
+    ################################################################################################
+
+    def init_new_test_handler(self):
+
+        def execute():
+            self.test = Test()
+            self.update_test_widget()
+            self.tabWidget.show()
+            self.setWindowTitle('EasyTest 2 - Новая аттестация')
+            self.save_as.setEnabled(True)
+
+            # NEW TEST INIT CODE
+
+        if self.test:
             reply = QW.QMessageBox.question(self,
                                             'Подтвердите дейтсвие',
                                             'Вы действительно хотите создать новую аттестацию? ' +
@@ -38,31 +60,72 @@ class EasyTest(QW.QMainWindow):
                                             QW.QMessageBox.No)
 
             if reply == QW.QMessageBox.Yes:
-                self.clear_test_widget()
-                self.test_active = True
-                self.tabWidget.show()
-                self.setWindowTitle('EasyTest 2 - Новая аттестация')
+                execute()
             else:
                 return
         else:
-            self.clear_test_widget()
-            self.test_active = True
-            self.tabWidget.show()
-            self.setWindowTitle('EasyTest 2 - Новая аттестация')
+            execute()
 
     def open_test_handler(self):
-        fname = QW.QFileDialog.getOpenFileName(self, 'Окрыть файл аттестации', '.')
 
+        def execute():
+            fname = QW.QFileDialog.getOpenFileName(self, 'Окрыть файл аттестации', '.')
+            if not fname[0]:
+                return
+            else:
+                try:
+                    self.test = Test(fname[0])  # init Test object
+                    self.update_test_widget()  # push data to UI
+                    self.tabWidget.show()
+                    self.setWindowTitle('EasyTest 2 - ' + fname[0])
+
+                    # OPEN TEST INIT CODE
+                    print('Opening test from {}'.format(fname[0]))
+                    self.save_as.setEnabled(True)
+                    self.save.setEnabled(True)
+
+                except Exception as e:
+                    print(e)
+
+        if self.test:
+            reply = QW.QMessageBox.question(self,
+                                            'Подтвердите дейтсвие',
+                                            'Вы действительно хотите открыть другую аттестацию? ' +
+                                            'Все несохранённые изменения будут потеряны!',
+                                            QW.QMessageBox.Yes | QW.QMessageBox.No,
+                                            QW.QMessageBox.No)
+
+            if reply == QW.QMessageBox.Yes:
+                execute()
+            else:
+                return
+        else:
+            execute()
+
+    def save_test_handler(self):
+        self.test.save()
+        self.statusBar().showMessage('Аттестация сохранена', 2000)
+
+    def save_test_as_handler(self):
+        fname = QW.QFileDialog.getSaveFileName(self, 'Выберите путь для сохранения', '.')
         if not fname[0]:
             return
         else:
             try:
-                print('Opening test from {}'.format(fname[0]))
+                self.test.save_as(fname[0] + '.json')
+                self.save.setEnabled(True)
+
             except Exception as e:
                 print(e)
 
-    def clear_test_widget(self):
-        print('Clearing widget')
+    # HELPERS
+    ################################################################################################
+
+    def update_test_widget(self):
+        pass
+
+    # EVENT OVERLOADING
+    ################################################################################################
 
     def closeEvent(self, event):
         reply = QW.QMessageBox.question(self,
